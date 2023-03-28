e2ad902a37e30153e36ca39aa22cda65a21b88dd
fasalasdms <fernando.salas@dms.ms>
2023-03-28 15:49:57 -0500

    Agregar funcion de guardado de mecánica

--- src/app/modules/admin/mecanica/mecanica.component.ts
+++ src/app/modules/admin/mecanica/mecanica.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { UserService } from 'app/core/user/user.service';
 import { User } from 'app/core/user/user.types';
+import { ApiService } from 'app/services/api.service';
 import { Subject, takeUntil } from 'rxjs';
 import { MecanicaService } from './mecanica.service';
 
@@ -40,7 +41,7 @@ export class MecanicaComponent implements OnInit {
   dataMecanica:any;
   user: User;
 
-  constructor(private _formBuilder: FormBuilder, private _mecanicaService:MecanicaService,private _userService: UserService,private _changeDetectorRef: ChangeDetectorRef) { }
+  constructor(private _formBuilder: FormBuilder, private _mecanicaService:MecanicaService,private _userService: UserService,private _changeDetectorRef: ChangeDetectorRef, private _apiService:ApiService) { }
 
   recalcular(){
     const valores = this.mecanicaForm.value;
@@ -52,6 +53,47 @@ export class MecanicaComponent implements OnInit {
     });
 
     this._mecanicaService.setMecanica(this.dataMecanica);
+  }
+
+  guardarMecanica(){
+
+    const mecanica = {
+      "fechaRegistro": "2023-03-28T19:58:17.931Z",
+      "idUsuario": this.user?.id,
+      "entradasxPuestoTrabajo": this.mecanicaForm?.value?.ent_x_pues_de_tra,
+      "pulmonesPorPuestoTrabajo": this.mecanicaForm?.value?.pul_por_pue_tra,
+      "puestosDeTrabajoCompletos": 0,
+      "elevadoresTotalesMecanica": this.mecanicaForm?.value?.ele_tot_mec,
+      "entradasPromedioTaller": this.mecanicaForm?.value?.ent_pro_tal_6_mes,
+      "tecnicosMecanicosElectromecanicos":  this.mecanicaForm?.value?.ent_pro_tal_6_mes,
+      "pulmones": this.mecanicaForm?.value?.pulmones,
+      "totalHorasFacturadas": this.mecanicaForm?.value?.tot_hor_fac,
+      "totalHorasTrabajadas": this.mecanicaForm?.value?.tot_hor_tra,
+      "totalHorasDisponibles": this.mecanicaForm?.value?.tot_hor_dis,
+      "horasHabilesLey": this.mecanicaForm?.value?.hor_hab_ley,
+      "tiempoProductivo": this.mecanicaForm?.value?.tie_prod,
+      "promedioHorasFacturacionEntrada": this.mecanicaForm?.value?.pro_hor_fac_por_ent,
+      "tasaEmpleo": 0,
+      "tasaEficiencia": 0,
+      "productividad": 0,
+      "aprovechamientoCapacidadServicio": 0,
+      "capacidadDeServicioInstalada": 0,
+      "tecnicosElevadoresTotales": 0,
+      "pulmonesElevadoresTotales": 0,
+      "pulmonesElevadoresProductivos": 0,
+      "entradasPuestoTrabajo": 0,
+      "entradasPotenciales": 0,
+      "region": "string",
+      "zona": "string",
+      "sociedad": "string",
+      "sede": "string",
+      "elevadoresProductivos": 0
+    };
+
+    this._apiService.postQuery("Mecanica/GuardarMecanica","",mecanica).subscribe(async(data:any)=>{
+      console.log("Resultado de guardar mecánica");
+    });
+
   }
 
   ngOnInit(): void {
